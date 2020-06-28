04b61f41ed99e5fd4c7ccf80575492297c66b4a8
fumn112026 <fumn112026@gmail.com>
2020-06-28 18:54:54 +0900

    スクロール機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -68,28 +68,27 @@ $(function(){
   
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        if (messages.length !== 0) {
-          var insertHTML = '';
-          $.each(messages, function(i, message) {
-            insertHTML += buildHTML(message)
-          });
-          $('messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-        }
-      })
-      .fail(function() {
-        alert('error');
-      });
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
   };
-  
-  if (document.location.href.match(/\/groups\/\d+\/messages)) {
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
 });
